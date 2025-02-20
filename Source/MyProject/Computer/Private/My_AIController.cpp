d97b562235bb30e8a16f6d527269c912207f5589
jeongsoll <80992713+jeongsoll@users.noreply.github.com>
2025-02-20 11:09:59 +0900

    controller 추가

---
+++ Source/MyProject/Computer/Private/My_AIController.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "My_AIController.h"
+
+#include "ComputerOne.h"
+#include "MapGen.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+AMy_AIController::AMy_AIController()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMy_AIController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	ComOne = Cast<AComputerOne>(UGameplayStatics::GetActorOfClass(GetWorld(), AComputerOne::StaticClass()));
+	map = Cast<AMapGen>(UGameplayStatics::GetActorOfClass(GetWorld(), AMapGen::StaticClass()));
+}
+
+// Called every frame
+void AMy_AIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/MyProject/Computer/Public/My_AIController.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/AIModule/Classes/AIController.h"
+#include "My_AIController.generated.h"
+
+UCLASS()
+class MYPROJECT_API AMy_AIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMy_AIController();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY()
+	class AComputerOne* ComOne;
+	UPROPERTY()
+	class AMapGen* map;
+
+	//탐색
+	//공격
+	//회피
+	//대기
+};
